39652635f9b36da8142195c2c66b08e50866852a
Saurav <souraw99@gmail.com>
2022-02-01 10:29:38 +0530

    web data

---
+++ crud/web/Show.php
@@ -0,0 +1,28 @@
+<html>
+
+<head>
+    <title>Show Records</title>
+</head>
+
+<body>
+    <table style="width:100%" rules="all" border="1">
+        <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>Email</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>SURAJ KUMAR MISHRA</td>
+            <td>surajmishra1872@gmail.com</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Sourav Joshi</td>
+            <td>sourav@gmail.com</td>
+        </tr>
+        
+    </table>
+</body>
+
+</html>
